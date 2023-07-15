accb6c71e95644d48094f9b31b8dc7ccc6e7e097
German Rodriguez <rodriguezgerman3298@gmail.com>
2023-07-15 08:04:31 +0200

    Adding commentaries to MillerRabin class.

--- FastExp.java
+++ FastExp.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class FastExp {
-    public static int fast_exponentiation (int base, int exp, int mod) {
+    public static int fast_mod_exponentiation (int base, int exp, int mod) {
         if (mod == 1) return 0;
         int value = 1;
         for (int i = 0; i < exp; i++)
@@ -9,7 +9,7 @@ public class FastExp {
         return value;
     }
 
-    public static int fast_exponentiation_binary_method (int base, int exp, int mod) {
+    public static int fast_mod_exponentiation_binary_method (int base, int exp, int mod) {
         if (mod == 1) return 0;
         //Check if (modulus -1) * (modulus - 1) does not overflow base
         int value = 1;
@@ -32,7 +32,7 @@ public class FastExp {
         System.out.println("Insert mod:");
         String mod = s.nextLine();
         s.close();
-        int result = fast_exponentiation_binary_method(Integer.parseInt(base), Integer.parseInt(exp), Integer.parseInt(mod));
+        int result = fast_mod_exponentiation_binary_method(Integer.parseInt(base), Integer.parseInt(exp), Integer.parseInt(mod));
         System.out.println("Solution: " + base + "^" + exp + " mod " + mod + " = " + result);
     }
 }
--- MillerRabin.java
+++ MillerRabin.java
@@ -5,30 +5,53 @@ import java.util.Scanner;
  * Miller-Rabin algorithm implementation,
  * with the binary method of fast modulo 
  * exponentiation
+ * Remember that Miller-Rabin returns "inconclusive"
+ * if the number can be prime, but it could also be
+ * composite.
+ * Repeating the algorithm with several bases (a's),
+ * it raises the probabilities to be prime (if returns 
+ * inconclusive in all tests) exponentially.
  */
 
 public class MillerRabin {
 
-    private int fast_exponentiation_binary_method (int base, int exp, int mod) {
+    /*
+     * Makes a fast modulo exponentiation 
+     * using succesive squaring binary method
+     * and modular arithmetic
+     * @return result of (base^exp modulo mod)
+     */
+    private int fast_mod_exponentiation_binary_method (int base, int exp, int mod) {
         if (mod == 1) return 0;
-        //Check if (mod - 1) * (mod - 1) doesn't overflow base
+        //Should check if (mod - 1) * (mod - 1) doesn't overflow base
         int value = 1;
         base = base % mod;
+        //Check binary digits one by one, and if it's odd (2^n), change value
         while (exp > 0){
             if (exp % 2 == 1)
                 value = (value * base) % mod;
-            exp = exp >> 1;
-            base = (base*base) % mod;
+            exp = exp >> 1;                     //always change binary number shifting to the right
+            base = (base*base) % mod;           //change base every step, using modular arithmetic 
         }
         return value;
     }
     
+    /*
+     * Miller-Rabin algorithm 
+     * Proves the primality of a number
+     * @param a Base used in a^(q*2^j) mod n 
+     * (where we check certain properties to know if it's a prime number)
+     * @param n Possible prime number to check
+     * @return "composite" if we are sure that isn't a prime number,
+     * or "inconclusive" if it could be a prime number
+     */
     private String miller_rabin_test (int a, int n) {
-        if (n % 2 == 0) return "composite";
-        //Find k and q
+        if (n % 2 == 0) return "composite";  //If number is even, obviously is a composite number
+        //Find k and q, k > 0 and q odd, so that n-1 = 2^k * q
         int q = 0;
         int k = 0;
         int nsub = n-1;
+        //Itering in k, looking that (n-1/2^k) = an integer odd number (q)
         while (q == 0) {
             k++;
             int pow = (int) Math.pow(2,k);
@@ -37,16 +60,20 @@ public class MillerRabin {
                 q = q % 2 == 1 ? q : 0;
             }
         }
-        //Check tests
-        if (fast_exponentiation_binary_method(a,q,n) == 1) return "inconclusive";
-        for (int i = 0; i < k; i++) {
-            int pow = (int) Math.pow(2,i);
-            if (fast_exponentiation_binary_method(a,q*pow,n) == nsub) 
+        //Check tests, checking certain prime numbers properties (self-explicated in code)
+        //If not passed, for sure it's a composite number
+        if (fast_mod_exponentiation_binary_method(a,q,n) == 1) return "inconclusive";
+        for (int j = 0; j < k; j++) {
+            int pow = (int) Math.pow(2,j);
+            if (fast_mod_exponentiation_binary_method(a,q*pow,n) == nsub) 
                 return "inconclusive";
         }
         return "composite";
     }
 
+    /*
+     * Basic program to test Miller-Rabin implementation
+     */
     public static void main (String[] args) {
         MillerRabin mr = new MillerRabin();
         Scanner s = new Scanner(System.in);
@@ -77,6 +104,7 @@ public class MillerRabin {
             System.out.println("Test results for number " + n + ": ");
             for (int i = 0; i < basesNumber; i++)
                 System.out.println((i+1) + ") With base " + bases[i] + ": " + mr.miller_rabin_test(bases[i], n));
+
         } else {
 
             System.out.println("Invalid option");
