e62b87d94c4e384bf6639c2df854fe044806e1f3
German Rodriguez <rodriguezgerman3298@gmail.com>
2023-07-20 09:49:43 +0200

    Reffine of CaesarCipherAffine. New version of the function to obtain the modular multiplicative inverse.

--- CaesarCipher.hs
+++ CaesarCipher.hs
@@ -13,17 +13,17 @@ caesarcipherDecrypt xs k = [toCharNorm (decrypt x) | x <- xs, isAsciiLower x]
 --Affine Caesar Cipher encrypt (only for lowercase texts)   C = (ap + b) mod 26
 --If a hasn't modulo 26 multiplicative inverse, the mapping won't be one to one
 caesarcipherAffineEncrypt :: String -> Int -> Int -> String
-caesarcipherAffineEncrypt xs a b = if hasModularMultiplicativeInverse a 26 then
-    [toCharNorm (encrypt x) | x <- xs, isAsciiLower x] 
-    else "The selected a has not modulo 26 multiplicative inverse."
+caesarcipherAffineEncrypt xs a b
+    | not $ hasModularMultiplicativeInverse a 26 = "The selected a has not modulo 26 multiplicative inverse."
+    | otherwise = [toCharNorm (encrypt x) | x <- xs, isAsciiLower x] 
     where encrypt x = (toIntNorm x * a + b) `mod` 26
 
 --Affine Caesar Cipher decrypt (only for lowercase texts)   p = a^-1*(c-b) mod 26
 --For this, we need to calcule the modular multiplicative inverse for a mod 26
 caesarcipherAffineDecrypt :: String -> Int -> Int -> String
-caesarcipherAffineDecrypt xs a b = if hasModularMultiplicativeInverse a 26 then
-    [toCharNorm (decrypt x) | x <- xs, isAsciiLower x]
-    else "The selected a has not modulo 26 multiplicative inverse."
+caesarcipherAffineDecrypt xs a b
+    | not $ hasModularMultiplicativeInverse a 26 = "The selected a has not modulo 26 multiplicative inverse."
+    | otherwise = [toCharNorm (decrypt x) | x <- xs, isAsciiLower x]
     where decrypt x = modularMultiplicativeInverse a 26 * (toIntNorm x - b) `mod` 26
 
 --Normalize a char, changing it to integer and transforming it from ASCII to our scale
@@ -51,6 +51,13 @@ eEA a b =
 get2nd :: (Int, Int, Int) -> Int
 get2nd (_,x,_) = x
 
+--Not efficient version, but for the mod 26 it is perfectly valid
+-- x starts at n-1 value 
+modularMultiplicativeInversev2 :: Int -> Int -> Int -> Int
+modularMultiplicativeInversev2 a n x
+    | a*x `mod` n == 1 = x
+    | otherwise = modularMultiplicativeInversev2 a n (x-1)
+
 --A number a has inverse modulo n if their mcd is 1
 hasModularMultiplicativeInverse :: Int -> Int -> Bool 
 hasModularMultiplicativeInverse a n = eA a n == 1
@@ -58,4 +65,4 @@ hasModularMultiplicativeInverse a n = eA a n == 1
 --Euclidean Algorithm
 eA :: Int -> Int -> Int
 eA a 0 = a
-eA a b = eA b (a `mod` b)eA
+eA a b = eA b (a `mod` b)
