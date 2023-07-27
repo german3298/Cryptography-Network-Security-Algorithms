cf1f494225dd8e4a6e643ee84ee4e05a96def41d
German Rodriguez <rodriguezgerman3298@gmail.com>
2023-07-27 13:30:56 +0200

    Not commited FrecuencyReader changes.

--- FrecuencyReader.java
+++ FrecuencyReader.java
@@ -6,33 +6,28 @@ public class FrecuencyReader {
      * @ author: Germán Rodríguez
      */
 
-     private double[] frecuenciesText; //relative frecuencies of each letter in the given text [0-25] [a-z]
      private double[] frecuenciesEnglish; //english relative frecuencies of each letter [0-25] [a-z] in English language
 
-     public double[] getTextFrecuencies () {
-        return frecuenciesText;
-     }
-
-     public double[] getEnglishLettersFrecuencies () {
+     public double[] getEnglishLettersFrecuencies() {
         return frecuenciesEnglish;
      }
 
-     public FrecuencyReader (String text){
-        frecuenciesText = new double[26];
+     public FrecuencyReader (){
         frecuenciesEnglish = new double[] {
-            8.167,1.492,2.782,4.253,12.702,2.228,2.015,6.094,6.996,0.153,0.772,
-            4.025,2.406,6.749,7.507,1.929,0.095,5.987,6.327,9.056,2.758,0.978,
-            2.360,0.150,1.974,0.074
+         0.081670,0.014920,0.027820,0.042530,0.127020,0.022280,0.020150,
+         0.060940,0.069960,0.001530,0.007720,0.040250,0.024060,0.067490,
+         0.075070,0.019290,0.000950,0.059870,0.063270,0.090560,0.027580,
+         0.009780,0.023600,0.001500,0.019740,0.000740
         };
-        ReadFrecuencies(text);
      }
 
      /*
       * Reads the frecuencies from the text and 
       * creates an array with the relative frecuencies
       */
-     public void ReadFrecuencies (String text) {
+     public double[] ReadFrecuencies (String text) {
         int[] frecuencies = new int[26];
+        double[] frecuenciesText = new double[26];
         text = text.toLowerCase().replaceAll("[^a-z]", "");   //Remove everything that is not a letter
         double lettersTotal = text.length();
         for (int i = 0; i < lettersTotal; i++){
@@ -41,16 +36,18 @@ public class FrecuencyReader {
             frecuencies[idx]++;
         }
         for (int i = 0; i < frecuenciesText.length; i++)
-            frecuenciesText[i] = (frecuencies[i]/lettersTotal)*100;
+            frecuenciesText[i] = (frecuencies[i]/lettersTotal);
+         return frecuenciesText;
      }
 
-     public void PrintFrecuencies () {
+     public void PrintFrecuencies (double[] frecuenciesText) {
         for (int i = 0; i < frecuenciesText.length; i++)
             System.out.println("Frecuency for letter " + (char)(i+'a') + " is " + frecuenciesText[i] + "%");
      }
 
      public static void main (String[] args){
-        FrecuencyReader FR = new FrecuencyReader(args[0]);
-        FR.PrintFrecuencies();
+        FrecuencyReader FR = new FrecuencyReader();
+        double[] frecuencies = FR.ReadFrecuencies(args[0]);
+        FR.PrintFrecuencies(frecuencies);
      }
 }
