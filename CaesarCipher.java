a94f5168eeb7acdac667a0d4f8b31d1e1870f7c9
German Rodriguez <rodriguezgerman3298@gmail.com>
2023-08-07 11:51:36 +0200

    -Afine of Vigenere and Caesar Cipher attacks.

---
+++ CaesarCipher.java
@@ -0,0 +1,19 @@
+public class CaesarCipher {
+
+    /*
+     * Simple CaesarCipher Decrypt
+     */
+    public String CaesarCipherDecrypt(String text, int key) {
+        text = text.toLowerCase().replaceAll("[^a-z]", "");
+        String decryptedText = "";
+        for (int i = 0; i < text.length(); i++) {
+            char p = text.charAt(i);
+            int numP = (p - key);
+            if (numP < 97)
+                numP += 26;
+            char c = (char) numP;
+            decryptedText += c;
+        }
+        return decryptedText;
+    }
+}
