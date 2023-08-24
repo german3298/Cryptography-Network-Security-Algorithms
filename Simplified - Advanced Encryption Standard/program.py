532f5115c69232b5e6979ff3b9655482349d2508
German Rodriguez <rodriguezgerman3298@gmail.com>
2023-08-24 17:53:20 +0200

    -File name changes and improvements to help in programs.

---
+++ Simplified - Advanced Encryption Standard/program.py
@@ -0,0 +1,31 @@
+import sys
+import saes
+
+#   Program to test my S-AES algorithm
+#   implementation based on strings
+#   with the binary representation of
+#   keys, plaintexts and ciphertexts.
+#
+#   @author: Germán Rodríguez
+
+if len(sys.argv) != 3:
+    print('Usage: %s <plain_text_binary> <key_binary> ' % sys.argv[0])  
+    print('Where: <plain_text_binary> is a string, only 16 bits in binary')
+    print('Where: <key_binary> is a string, only 16 bits in binary')
+    print('Example: python3 program.py "meetmeat" "9234AB47C3428476"')
+    sys.exit(1)
+
+binary_plain_text = sys.argv[1]
+binary_key = sys.argv[2]
+
+print()
+print("Plaintext: " + binary_plain_text)
+print()
+
+encrypted_binary_text = saes.encrypt(binary_plain_text,binary_key)
+print("Encrypted binary text: " + encrypted_binary_text)
+print()
+
+decrypted_binary_text = saes.decrypt(encrypted_binary_text,binary_key)
+print("Decrypted binary text: " + decrypted_binary_text)
+print()
